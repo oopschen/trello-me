ee06fd4c2e642c96481f0ee2ae79fb391ecbeb11
Ray <linxray@gmail.com>
2022-06-25 17:45:03 +0800

    feat: add filter cards in board & list

--- command/filter_board_card.py
+++ command/filter_board_card.py
@@ -4,6 +4,7 @@ import click as ck
 
 from trellome import board as bd
 from trellome import member as mb
+from trellome import trello_list as tlist
 
 from . import setup as st
 
@@ -11,8 +12,10 @@ from . import setup as st
 @ck.option('-t', '--token', required=True, help='Trello api token.')
 @ck.option('-k', '--key', required=True, help='Trello api key.')
 @ck.option('-n', '--name', help='Board list name regex.')
+@ck.option('-f', '--list-filter', help='Board List filter.', default="closed",
+   type=ck.Choice(['all', 'closed', 'none', 'open'], case_sensitive=False))
 @ck.argument('board_name')
-def filter_archived_board_list(board_name, token, key, name):
+def filter_board_list(board_name, token, key, name, list_filter):
   st.global_setup(key, token)
 
   boards = mb.get_boards("me", board_name)
@@ -21,7 +24,7 @@ def filter_archived_board_list(board_name, token, key, name):
     return
 
   for board in boards:
-    trello_list = bd.get_boards_list(board["id"], "closed", name)
+    trello_list = bd.get_boards_list(board["id"], list_filter, name)
 
     board_id = board["id"]
     board_name = board["name"]
@@ -37,3 +40,47 @@ def __print_trello_list(trello_list):
 
   for idx, val in enumerate(trello_list):
     print(f"No.{idx+1}\t(id,name)\t({val['id']} {val['name']})")
+
+@ck.command()
+@ck.option('-t', '--token', required=True, help='Trello api token.')
+@ck.option('-k', '--key', required=True, help='Trello api key.')
+@ck.option('-ln', '--list-name', help='Board list name regex.')
+@ck.option('-n', '--name', help='Board card name regex.')
+@ck.option('-f', '--list-filter', help='Board List filter.', default="closed",
+   type=ck.Choice(['all', 'closed', 'none', 'open'], case_sensitive=False))
+@ck.argument('board_name')
+def filter_board_card(board_name, token, key, list_name, name, list_filter):
+  st.global_setup(key, token)
+
+  boards = mb.get_boards("me", board_name)
+  if None == boards:
+    print("No boards found")
+    return
+
+  for board in boards:
+    trello_list = bd.get_boards_list(board["id"], list_filter, list_name)
+
+    board_id = board["id"]
+    board_name = board["name"]
+
+    print(f"---- Start Board '{board_name}({board_id})' List -----")
+
+    for idx, val in enumerate(trello_list):
+      list_id = val["id"]
+      list_name = val["name"]
+      print(f"------ No.{idx+1} List '{list_name}({list_id})' List -------")
+      card_list = tlist.get_card(list_id, name)
+      __print_trello_card(card_list)
+      print(f"------ End List '{list_name}({list_id})' List -------\n")
+
+    print(f"---- End Board '{board_name}({board_id})' List -----")
+
+def __print_trello_card(trello_card):
+  if None == trello_card:
+    print("No trello card found.")
+    return
+
+  for idx, val in enumerate(trello_card):
+    print(f"""No.{idx+1}\t(id,name,desc,shortUrl,address)\t({val['id']} {val['name']} {val['desc']}
+        {val['shortUrl']} {"address" in val and val['address']})""")
+
